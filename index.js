fb6d8d17e81401c64bf88c9bebdc4be8d895b75e
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-01-09 00:04:01 +0700

    Initial seed

---
+++ index.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+
+// Register static files here
+// -- Example: app.use("/semantic",express.static(__dirname + "/semantic"));
+
+app.use("/public",express.static(__dirname + "/public"));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
