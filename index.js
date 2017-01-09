19509ad739c37ecadd80046c6fd8891dac5fd6cd
RING2-CORP\febrinanda.pratama <febrinanda.pratama@erraipasifik.com>
2017-01-09 12:07:18 +0700

    ui form and image

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/page2', function(req, res){
+  res.sendFile(__dirname + '/page2.html');
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
